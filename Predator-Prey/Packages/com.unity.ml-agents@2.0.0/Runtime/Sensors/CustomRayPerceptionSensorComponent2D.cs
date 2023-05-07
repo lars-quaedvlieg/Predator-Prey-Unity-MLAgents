7661dc874d51f5e5cc86b638fe5b1d210acc9c49
Somesh Mehra <someshmehra99@gmail.com>
2023-05-07 20:27:10 +0200

    Create custom ray perception sensor scripts

---
+++ Predator-Prey/Packages/com.unity.ml-agents@2.0.0/Runtime/Sensors/CustomRayPerceptionSensorComponent2D.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Unity.MLAgents.Sensors
+{
+    /// <summary>
+    /// A component for 2D Ray Perception.
+    /// </summary>
+    [AddComponentMenu("ML Agents/Ray Perception Sensor 2D", (int)MenuGroup.Sensors)]
+    public class CustomRayPerceptionSensorComponent2D : CustomRayPerceptionSensorComponentBase
+    {
+        /// <inheritdoc/>
+        public override CustomRayPerceptionCastType GetCastType()
+        {
+            return CustomRayPerceptionCastType.Cast2D;
+        }
+    }
+}
